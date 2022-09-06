3a112b4f1359e21e5bdda72dbd658d149cdafdb8
Dabee-ee <movie2529@naver.com>
2022-09-06 11:22:28 +0900

    Updates

---
+++ precache-manifest.d20fa4a6ad8f1f99b5ce9dfc1af886ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db9b6107dbff5c8e5253192a0ce6d5c",
+    "url": "/class-movie-app/index.html"
+  },
+  {
+    "revision": "a2e8f16b23df75caa96c",
+    "url": "/class-movie-app/static/css/main.41724497.chunk.css"
+  },
+  {
+    "revision": "1848515d581c5436eb30",
+    "url": "/class-movie-app/static/js/2.4725272f.chunk.js"
+  },
+  {
+    "revision": "a2e8f16b23df75caa96c",
+    "url": "/class-movie-app/static/js/main.91012d19.chunk.js"
+  },
+  {
+    "revision": "02c2a175969e4348b60c",
+    "url": "/class-movie-app/static/js/runtime~main.280a2705.js"
+  }
+]);
